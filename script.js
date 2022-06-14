b0fdd8f60175e51a047f8413b662f2500ae0faac
Surur Khan <surur.rkhan@gmail.com>
2022-06-14 14:10:02 -0400

    Added rematch functionality

--- script.js
+++ script.js
@@ -25,7 +25,12 @@ const gameBoard = (() => {
         return board[index];
     };
 
-    return { setField, getField };
+    const rematch = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    }
+    return { setField, getField, rematch };
 })();
 
 // Module for game display control //
@@ -33,7 +38,7 @@ const gameBoard = (() => {
 const displayController = (() => {
     const fieldElements = document.querySelectorAll(".field");
     const messageElement = document.querySelector(".message");
-    const rematchElement = document.querySelector(".rematch");
+    const rematchButton = document.getElementById("rematch");
     const resetElement = document.getElementById('reset');
 
     // Reset entire webpage //
@@ -43,15 +48,31 @@ const displayController = (() => {
         location.reload();
     };
 
-    // Display winner result //
-    const SetResultMessage = (winner) => {
-        if (winner === "Draw") {
+    // Display player result //
+    const SetResultMessage = (res) => {
+        if (res === "Draw") {
             messageElement.textContent = "Draw!";
         } else {
-            messageElement.textContent = `${winner} has won!`;
+            messageElement.textContent = `${res} has won!`;
         };
     };
 
-    return {};
+    // Changes gameboard //
+    const updateGameboard = () => {
+        for (let i = 0; i < fieldElements.length; i++) {
+            fieldElements[i].textContent = gameBoard.getField(i);
+        }
+    };
+
+    // Rematch after round played //
+    rematchButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        gameBoard.rematch();
+        updateGameboard();
+        messageElement.textContent = "Player X Turn";
+
+        return { SetResultMessage };
+    });
 })();
+
 
